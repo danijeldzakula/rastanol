d7c1b67e5862740159ffd3deab42d153d71ca850
Dado19Git <danidzaki060@gmail.com>
2020-01-19 20:45:53 +0100

    Little changes

--- js/main.js
+++ js/main.js
@@ -6,12 +6,3 @@
 }
 
 window.addEventListener('scroll', ScrollIndicator);
-
-
-window.addEventListener('load',function(){
-	window.scrollTo(0, 0);
-	var el = document.querySelector('body');
-	setTimeout(function() {
-		el.classList.add("loaded");
-	}, 700);	
-});
