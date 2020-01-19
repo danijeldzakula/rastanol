1994f0431040f4f4bfe33f5ec13a79d01f4db088
Dado19Git <danidzaki060@gmail.com>
2020-01-19 18:36:16 +0100

    First commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+﻿function ScrollIndicator() {
+	var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+	var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+	var scrolled = (winScroll / height) * 100;
+	document.getElementById('scrollIndicator').style.width = scrolled + "%";
+}
+
+window.addEventListener('scroll', ScrollIndicator);
+
+
+window.addEventListener('load',function(){
+	window.scrollTo(0, 0);
+	var el = document.querySelector('body');
+	setTimeout(function() {
+		el.classList.add("loaded");
+	}, 700);	
+});
